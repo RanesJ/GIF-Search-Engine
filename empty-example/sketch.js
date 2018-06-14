f5f0dfb434b36a75a64d44e371fa1c9f35a68f48
ranes <rls25@gmail.com>
2018-06-14 11:32:05 -0400

    added input box Users can now enter a keyword in the input box and a gif will return

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,13 +1,18 @@
 var api = "http://api.giphy.com/v1/gifs/search?";
 //This is GIPHY public key that anyone can use. 
 var apiKey = "&api_key=dc6zaTOxFJmzC";
-var query = "&q=hi";
+//var query = "&q=rainbow";
 
 function setup() {
     noCanvas();
-    var url = api + apiKey + query;
+    var button = select('#submit');
+    button.mousePressed(userAsk);
+    
+    input = select('#city');
+}
+function userAsk() {
+    var url = api + apiKey + '&q=' + input.value();
     loadJSON(url, gotData);
-
 }
 function gotData(giphy) {
     for (var i = 0 ; i < giphy.data.length; i++) {
