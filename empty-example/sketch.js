79f1a2ff113055bc7aba2d5b331b54086a0afdda
ranes <rls25@gmail.com>
2018-06-15 12:48:33 -0400

    delete and add new gif When user tyoes a new keyword nad hits "enter" the old gif will be replaced with a new one.Also cneter everything

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,23 +1,53 @@
 var api = "http://api.giphy.com/v1/gifs/search?";
 //This is GIPHY public key that anyone can use. 
 var apiKey = "&api_key=dc6zaTOxFJmzC";
-//var query = "&q=rainbow";
-
+var test = 0;
 function setup() {
     noCanvas();
     var button = select('#submit');
-    button.mousePressed(userAsk);
-    
-    input = select('#city');
+    button.mousePressed(userAsk); 
+    input = select('#keyword');
 }
+//wil remove the 
+
+function buttonPressed() {
+    
+  
+
+}
+
+
 function userAsk() {
     var url = api + apiKey + '&q=' + input.value();
     loadJSON(url, gotData);
+
 }
 function gotData(giphy) {
-    for (var i = 0 ; i < giphy.data.length; i++) {
-    createImg(giphy.data[i].images.original.url);
-}
+    
+    if (test === 0){
+         var images = document.getElementsByTagName('img');
+        var l = images.length;
+        for (var i = 0; i < l; i++) {
+            images[0].parentNode.removeChild(images[0]);
+
+        }
+//   for (var i = 0 ; i < giphy.data.length; i++) {
+    createImg(giphy.data[0].images.original.url);
+            test = 1;
+    }
+    else{
+        var images = document.getElementsByTagName('img');
+        var l = images.length;
+        for (var i = 0; i < l; i++) {
+            images[0].parentNode.removeChild(images[0]);
+
+        }
+
+            test = 0;
+            createImg(giphy.data[0].images.original.url);
+
+        
+    }
 }
 
 
